3cf6e3b5caf79b037bbe58c0c4246fb87ce4dfd0
Gabriel Arbigaus <gabriel.arbigaus@stratesys-ts.com>
2024-04-10 19:56:52 -0300

    Adicionando questões do desafio

--- Curso_Dart/funcoes/desafio.dart
+++ Curso_Dart/funcoes/desafio.dart
@@ -1,24 +1,28 @@
 //! Detalhe sobre a String
 //! A String é composta por 4 campos (Nome|Idade|Profissão|Estado)
 //! Baseado no array acima monte um relatório onde
-//! 1- Inclua o nome, idade, 
-//! 2- 
-//! 3- 
-//! 4- 
-//! 5- 
+//! 1- Inclua o nome, idade, profissão e estado de origem dos participantes do grupo no array
+//! 2- Apresente a quantidade total de pacientes do array
+//! 3- Apresente os pacientes com mais de 20 anos e print o nome deles
+//! 4- Apresente quantos pacientes existem para cada profissão (desenvolvedor, estudante, dentidata, jornalista)
+//! 5- Apresente a quantidade de pacientes que moram em SP
+
 //! Enviar no e-mail profbeto.silva@fiap.com.br
 
 void main(List<String> args) {
   final pacientes = [
-    'Rodrigo Rahman | 35 | Desenvolvedor | SP',
-    'Manoel Silva | 12 | Estudante | AL',
-    'Joaquim Rahman| 18 | Estudante | SP',
-    'Fernando Verne | 35 | Estudante | PE',
-    'gustavo Silva | 40 | Desenvolvedor | CE',
-    'Sandra Silva | 40 | Desenvolvedor | MG',
-    'Rgina Verne | 35 | Dentista | MG',
-    'Joao Rahman | 55 | Jornalista | RJ',
+    'Rodrigo Rahman|35|Desenvolvedor|SP',
+    'Manoel Silva|12|Estudante|AL',
+    'Joaquim Rahman|18|Estudante|SP',
+    'Fernando Verne|35|Estudante|PE',
+    'gustavo Silva|40|Desenvolvedor|CE',
+    'Sandra Silva|40|Desenvolvedor|MG',
+    'Rgina Verne|35|Dentista|MG',
+    'Joao Rahman|55|Jornalista|RJ',
+    'Gabriel Arbigaus|21|Desenvolvedor|SC',
+    'Guilherme Cardoso|19|Desenvolvedor|SP',
   ];
+
 
   print(pacientes);
 }
