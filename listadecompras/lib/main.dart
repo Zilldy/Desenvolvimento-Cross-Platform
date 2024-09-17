fdc604ba21d6acb51fa33c4006f4c16d19d12f75
zilldy <guicardosobarreiro1@gmail.com>
2024-09-16 21:30:02 -0300

    Refatorando o main.dart

--- listadecompras/lib/main.dart
+++ listadecompras/lib/main.dart
@@ -2,5 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:untitled/screens/fruit_list.screen.dart';
 
 void main() {
-  runApp(const FruitListScreen());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const FruitListScreen(),
+    );
+  }
 }
