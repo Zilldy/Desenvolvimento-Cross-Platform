8a8bc8f595c3f98fb58e24f72d69dd793e34436d
zilldy <guicardosobarreiro1@gmail.com>
2024-09-30 20:26:45 -0300

    Aula 30/09 - Persistencia and Shared Preference

--- README.md
+++
@@ -1,2 +0,0 @@
-# Desenvolvimento-Cross-Platform
-Flutter Class
---
+++ loginscreen/lib/screens/controller/login.controller.dart
@@ -0,0 +1,48 @@
+import '../../datasource/shared_preference.datasource.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  final Rx<String?> _name = Rx(null);
+  final Rx<String?> _password = Rx(null);
+  final Rx<String?> message = Rx(null);
+  final Rx<bool> logged = Rx(false);
+  late SharedPreferenceDataSource _preference;
+
+  LoginController() {
+    initController();
+  }
+
+  String get name => _name.value ?? "";
+
+  String get password => _password.value ?? "";
+
+  initController() async {
+    _preference = SharedPreferenceDataSource();
+    await _preference.initPreference();
+    logged.value = await _preference.isLogged();
+  }
+
+  save(String email, String password) async {
+    bool isValid = true;
+
+    if (email.isEmpty) {
+      message.value = "O nome não pode ser vazio";
+      isValid = false;
+    }
+
+    if (password.isEmpty) {
+      message.value = "O email não pode ser vazio";
+      isValid = false;
+    }
+
+    if (isValid) {
+      String? resEmail = await _preference.load("email");
+      String? resPassword = await _preference.load("password");
+      if (email == resEmail && password == resPassword) {
+        logged.value = true;
+      } else {
+        message.value = "E-mail ou password incorreto!";
+      }
+    }
+  }
+}
